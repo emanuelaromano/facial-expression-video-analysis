b1495fb0567a65ce94ca9f2f26161e2cbca407cb
emanuelaromano98 <emauelaromano1998@gmail.com>
2025-08-25 18:13:15 -0400

    Update

--- frontend/api.jsx
+++ frontend/api.jsx
@@ -1,9 +1,9 @@
-const status = "dev";
+const status = "prod";
 export const API_URL =
   status === "prod"
-    ? "https://backend-app-101856457372.us-central1.run.app"
+    ? "https://hireview-prep-916307297241.us-central1.run.app"
     : "http://localhost:8080";
 export const WS_URL =
   status === "prod"
-    ? "wss://backend-app-101856457372.us-central1.run.app"
+    ? "https://hireview-prep-916307297241.us-central1.run.app"
     : "ws://localhost:8080";
